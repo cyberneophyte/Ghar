7556536eee874a63aff2bda33d0a4a220e2e4559
cyberneophyte <mdanishriaz@gmail.com>
2019-12-21 18:46:37 -0500

    User prompt example 1

---
+++ User_prompt1.py
@@ -0,0 +1,8 @@
+print("Hi!")
+name= input("Whats's your name? ")
+print("Its nice to meet you,",name)
+ans = input("Are you enjoying this course? ")
+if ans=="Yes":
+    print("That's good to hear!")
+else:
+    print("That sucks!")
