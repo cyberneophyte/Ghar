e14b4a4aa87ac429d71966004f3e3b4ebbb87ec9
cyberneophyte <mdanishriaz@gmail.com>
2020-01-02 20:31:14 -0500

    Files added from linkedin exercise

---
+++ car_wash.py
@@ -0,0 +1,12 @@
+def car_wash(amount_paid):
+    if(amount_paid==12):
+        print("Wash with tri-color foam.")
+        print("Rinse twice.")
+        print("Dry with a large blow dryer.")
+    if(amount_paid==6):
+        print("Wash wit white foam.")
+        print("Rinse once.")
+        print("Air dry.")
+
+
+car_wash(6)
---
+++ guess.py
@@ -0,0 +1,7 @@
+food= input("What's your favorite food? ")
+if food =='Ice cream':
+    print("Yep,So amazing!")
+else:
+    print("Yuck! That's not it!")
+
+print("Thanks for playing!")
